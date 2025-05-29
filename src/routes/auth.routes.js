f01812653c2f6b7dd1802795dfa965daf5fce799
BeygonK <kipkoechbeygon@gmail.com>
2025-05-30 00:44:03 +0300

    fixed routes mismatch

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const {
   register,
   login,
-  getCurrentUser,
+  getMe,
   registerAdmin,
 } = require("../controllers/auth.controller");
 const { protect } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", protect, getCurrentUser);
+router.get("/me", protect, getMe);
 
 // Temporary admin registration route (REMOVE THIS IN PRODUCTION)
 router.post("/register-admin", registerAdmin);
